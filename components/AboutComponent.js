05af33a24577a3ec3af8a97abff8d2dc919aaa33
Raphael Ernest <raphael.ernest702@gmail.comf>
2021-04-16 22:45:12 -0700

    Acticity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import {  Text, ScrollView,FlatList } from 'react-native';
 import { Card,ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -30,17 +31,41 @@ class About extends Component {
     static navigationOptions = {
         title: 'About Us'
     }
-    render() {
-        const renderPartner =({item}) => {
-        return (
-            <ListItem
-                title={item.name}
-                subtitle={item.description}
-                leftAvatar={{source: {uri: baseUrl + item.image}}}
-            />
-        );
-    }
 
+    
+    render() {
+        const renderPartner = ({item}) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
+                />
+            );
+        };
+
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
 
                
     
