27a764a26af36f9cfa1fefcbdeddb7f8a1198216
Raphael Ernest <raphael.ernest702@gmail.comf>
2021-04-27 09:49:10 -0700

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -4,6 +4,7 @@ import { Text, View, ScrollView, StyleSheet,
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
 import { Alert } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -45,7 +46,9 @@ class Reservation extends Component {
                         },
                         {
                             text: 'OK',
-                            onPress: () => {this.resetForm()}
+                            onPress: () => {
+                                this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                                this.resetForm()}
                         }
                      ],
                      {
@@ -64,7 +67,31 @@ class Reservation extends Component {
         });
     }
 
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
 
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    }
 
 
     render() {
